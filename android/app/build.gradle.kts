96bfebc783222c840db7c7fa36f06315d4ad316b
diaaldin <dia81994@gmail.com>
2025-09-10 13:54:36 +0300

    release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,17 +29,17 @@ android {
 
     signingConfigs {
         release {
-            keyAlias 'bastah-key-alias'
-            keyPassword 'diakrea00'
-            storeFile file('/full/path/to/my-release-key.jks')
-            storePassword 'diakrea00'
+            keyAlias = 'bastah-key-alias'
+            keyPassword = 'diakrea00'
+            storeFile = file('/full/path/to/my-release-key.jks')
+            storePassword = 'diakrea00'
         }
     }
 
     buildTypes {
         release {
-            signingConfig signingConfigs.release
-            minifyEnabled false
+            signingConfig = signingConfigs.release
+            minifyEnabled = false
         }
     }
 }
